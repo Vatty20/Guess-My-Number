a664d94e97761c30f4f2c1f46b349a7c73d79641
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-21 22:05:30 +0530

    implementing logic

--- script.js
+++ script.js
@@ -12,12 +12,39 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc will remove decimal digits
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //   console.log(guess); --> This value will be in string, so convert it to number
   console.log(guess, typeof guess);
 
+  // code to check secret number and change score as needed
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number!!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☹️ Oops! Too high.';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😓 You lost the game!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☹️ Oops! Too low.';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😓 You lost the game!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   }
 });
