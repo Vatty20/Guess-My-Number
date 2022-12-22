cc39e77735f7f26b73d1738b2c65848279a0b9bf
Vatsal Harlalka <harlalka.vatsal@gmail.com>
2022-12-22 22:26:10 +0530

    improving game

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc will remove decimal digits
+let secretNumber = Math.trunc(Math.random() * 20) + 1; //trunc will remove decimal digits
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +27,17 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!!';
+    // if (score > document.querySelector('.highscore').textContent) {
+    //   document.querySelector('.highscore').textContent = score;
+    // }
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '☹️ Oops! Too high.';
@@ -47,4 +59,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.guess').value = '';
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+  });
 });
